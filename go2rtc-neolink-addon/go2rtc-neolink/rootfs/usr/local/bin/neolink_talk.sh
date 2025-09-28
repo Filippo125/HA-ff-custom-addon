875cd9410bf560922d99c11d871a1805c81d5147
Filippo125 <filippo.ferrazini@gmail.com>
2025-09-28 19:44:35 +0200

    test build

--- go2rtc-neolink-addon/go2rtc-neolink/rootfs/usr/local/bin/neolink_talk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Read A-Law 8kHz mono from STDIN (WebRTC backchannel) -> WAV -> neolink talk
-set -euo pipefail
-
-ffmpeg -hide_banner -loglevel error -fflags nobuffer \
-  -f alaw -ar 8000 -ac 1 -i - -f wav -  \
-| neolink talk Doorbell -c /etc/neolink.toml -m -i "fdsrc fd=0"
